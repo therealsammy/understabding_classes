ae359b33592dc759ca169fdee3d2bebccb0fad99
Samuel Ohiri <therealsammy18x@gmail.com>
2022-12-28 10:30:27 +0100

    files restructure and reademe update

---
+++ inheritance/Developer.py
@@ -0,0 +1,25 @@
+from Employee import Employee
+
+
+class Developer(Employee):
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, programming_language):
+        super().__init__(first, last, pay)
+        #Employee.__init__(self, first, last, pay)
+        self.programming_language = programming_language
+
+
+
+# print(help(Developer))
+
+
+
+
+
+
+
+# dev_1 = Developer("Sam", "Oh", 12000, "Python")
+# dev_2 = Developer("Akin1", "Bello", 24000, "C")
+
+# print(dev_2.programming_language)
--- inheritance/tests/test_employee.py
+++ inheritance/tests/test_employee.py
@@ -2,6 +2,7 @@ import unittest
 from Employee import Employee
 
 
+
 class TestEmployee(unittest.TestCase):
 
     def setUp(self):
